aa79905a5045ada95196a52389adfc5390ccd537
Bernardo Farah <bernardo.farah@samsara.com>
2018-11-20 14:59:13 -0800

    schemabuilder: pull context through to filters and sorts

--- graphql/schemabuilder/pagination.go
+++ graphql/schemabuilder/pagination.go
@@ -411,7 +411,7 @@ func matchString(str, query string) bool {
 	return false
 }
 
-func (c *connectionContext) applyTextFilter(nodes []interface{}, args PaginationArgs) ([]interface{}, error) {
+func (c *connectionContext) applyTextFilter(ctx context.Context, nodes []interface{}, args PaginationArgs) ([]interface{}, error) {
 	if args.Filter == nil || *args.Filter == "" {
 		return nodes, nil
 	}
@@ -422,7 +422,7 @@ func (c *connectionContext) applyTextFilter(nodes []interface{}, args Pagination
 		// For each possible field we're matching against, check if there's a match.
 		for _, filterField := range c.FilterTextFields {
 			// Resolve the graphql.Field made for sorting.
-			text, err := filterField.Resolve(context.TODO(), node, nil, nil)
+			text, err := filterField.Resolve(ctx, node, nil, nil)
 			if err != nil {
 				return nil, err
 			}
@@ -430,7 +430,7 @@ func (c *connectionContext) applyTextFilter(nodes []interface{}, args Pagination
 			// Only strings are allowed for FilterText fields.
 			textString, ok := text.(string)
 			if !ok {
-				return nil, errors.New("Text was not a string")
+				return nil, errors.New("filter field didn't return a string")
 			}
 
 			if matchString(textString, *args.Filter) {
@@ -447,7 +447,7 @@ func (c *connectionContext) applyTextFilter(nodes []interface{}, args Pagination
 	return filteredNodes, nil
 }
 
-func (c *connectionContext) applySort(nodes []interface{}, args PaginationArgs) ([]interface{}, error) {
+func (c *connectionContext) applySort(ctx context.Context, nodes []interface{}, args PaginationArgs) ([]interface{}, error) {
 	if args.SortBy == nil {
 		return nodes, nil
 	}
@@ -461,7 +461,7 @@ func (c *connectionContext) applySort(nodes []interface{}, args PaginationArgs) 
 	sortField, ok := c.SortFields[*args.SortBy]
 	// If the field wasn't registered, it's an unknown sort field.
 	if !ok {
-		return nil, fmt.Errorf("Unknown sort field %s", *args.SortBy)
+		return nil, fmt.Errorf("unknown sort field %s", *args.SortBy)
 	}
 
 	// sortValues is the slice we'll be sorting (with the sorted values) in order to figure out
@@ -469,7 +469,7 @@ func (c *connectionContext) applySort(nodes []interface{}, args PaginationArgs) 
 	sortValues := make([]sortReference, len(nodes))
 	for i, node := range nodes {
 		// Resolve the graphql.Field made for sorting.
-		sortValue, err := sortField.Resolve(context.TODO(), node, nil, nil)
+		sortValue, err := sortField.Resolve(ctx, node, nil, nil)
 		if err != nil {
 			return nil, err
 		}
@@ -494,7 +494,7 @@ func (c *connectionContext) applySort(nodes []interface{}, args PaginationArgs) 
 
 // getConnection applies the ConnectionArgs to nodes and returns the result in a wrapped Connection
 // type.
-func (c *connectionContext) getConnection(out []reflect.Value, args PaginationArgs) (Connection, error) {
+func (c *connectionContext) getConnection(ctx context.Context, out []reflect.Value, args PaginationArgs) (Connection, error) {
 	nodes := castSlice(out[0].Interface())
 	if len(nodes) == 0 {
 		return Connection{}, nil
@@ -502,11 +502,11 @@ func (c *connectionContext) getConnection(out []reflect.Value, args PaginationAr
 
 	if !c.IsExternallyManaged() {
 		var err error
-		nodes, err = c.applyTextFilter(nodes, args)
+		nodes, err = c.applyTextFilter(ctx, nodes, args)
 		if err != nil {
 			return Connection{}, nil
 		}
-		nodes, err = c.applySort(nodes, args)
+		nodes, err = c.applySort(ctx, nodes, args)
 		if err != nil {
 			return Connection{}, nil
 		}
@@ -876,7 +876,7 @@ func (sb *schemaBuilder) buildPaginatedField(typ reflect.Type, m *method) (*grap
 			// Call the function.
 			out := fun.Call(in)
 
-			return c.extractReturnAndErr(out, args, retType)
+			return c.extractReturnAndErr(ctx, out, args, retType)
 
 		},
 		Args:           args,
@@ -888,7 +888,7 @@ func (sb *schemaBuilder) buildPaginatedField(typ reflect.Type, m *method) (*grap
 	return ret, nil
 }
 
-func (c *connectionContext) extractReturnAndErr(out []reflect.Value, args interface{}, retType graphql.Type) (interface{}, error) {
+func (c *connectionContext) extractReturnAndErr(ctx context.Context, out []reflect.Value, args interface{}, retType graphql.Type) (interface{}, error) {
 	var paginationArgs PaginationArgs
 
 	// If the pagination args are not embedded then they need to be extracted out of ConnectionArgs
@@ -908,7 +908,7 @@ func (c *connectionContext) extractReturnAndErr(out []reflect.Value, args interf
 		paginationArgs = reflect.ValueOf(args).Field(c.PaginationArgsIndex).Interface().(PaginationArgs)
 	}
 
-	result, err := c.getConnection(out, paginationArgs)
+	result, err := c.getConnection(ctx, out, paginationArgs)
 	if err != nil {
 		return nil, err
 	}
